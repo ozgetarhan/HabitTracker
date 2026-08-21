ba0c7a03729242f535ee510beaf9d0b899932131
ozgetarhan <ozgeee.tarhan@gmail.com>
2026-08-21 15:35:10 +0300

    Add AlertDialog to AddHabitActivity

--- app/src/main/java/com/example/habittracker/AddHabitActivity.kt
+++ app/src/main/java/com/example/habittracker/AddHabitActivity.kt
@@ -1,9 +1,11 @@
 package com.example.habittracker
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -20,12 +22,28 @@ class AddHabitActivity : AppCompatActivity() {
 
         binding.btnSave.setOnClickListener {
             val habitName = binding.etHabitName.text.toString().trim()
+
             if (habitName.isNotEmpty()) {
-                val intent = Intent(this@AddHabitActivity, MainActivity::class.java)
-                intent.putExtra("HABIT_NAME", habitName)
-                startActivity(intent)
-                finish()
-            }
+                val alert = AlertDialog.Builder(this)
+                alert.setTitle("Kayıt Et")
+                alert.setMessage("Kayıt etmek istediğinize emin misiniz?")
+
+                alert.setPositiveButton("Evet") { dialog, which ->
+                    Toast.makeText(this, "Kayıt edildi", Toast.LENGTH_LONG).show()
+
+                    // "Evet" denilince Intent ile MainActivity'ye dön
+                    val intent = Intent(this@AddHabitActivity, MainActivity::class.java)
+                    intent.putExtra("HABIT_NAME", habitName)
+                    startActivity(intent)
+                    finish()
+                }
+
+                alert.setNegativeButton("Hayır") { dialog, which ->
+                    Toast.makeText(this, "Kayıt iptal edildi", Toast.LENGTH_LONG).show()
+                }
+
+                // Dialogu ekranda göstermek için:
+                alert.show()}
             else{
                 Toast.makeText(this, "Lütfen bir alışkanlık girin!", Toast.LENGTH_SHORT).show()
             }
